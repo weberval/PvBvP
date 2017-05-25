2341d30a9bc5ae90e2f28f4e521d65513f1fedf3
Renat Sakenov <renat.sakenov@googlemail.com>
2017-05-25 18:55:45 +0200

    new draft, now with datagram sockets (udp)

--- app/src/main/java/de/dhbw_loerrach/pvbvp/Network/Networking.java
+++ app/src/main/java/de/dhbw_loerrach/pvbvp/Network/Networking.java
@@ -2,10 +2,9 @@ package de.dhbw_loerrach.pvbvp.Network;
 
 import android.util.Log;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 /**
  * Created by renat on 23.05.17.
@@ -19,61 +18,95 @@ public class Networking {
     public static boolean SERVER = false;
     public static int PORT = 4567;
 
-    public static boolean SERVER_RUNNING = true;
+    public static boolean CLIENT_CONNECTED = false;
 
-    private static ServerSocket serverSocket;
-    private static Socket clientSocket;
+    private static DatagramSocket socket;
+    private static DatagramSocket socketReceive;
 
+    private static InetAddress partnerAddress;
 
-
-    public static void startClient(){
-
-    }
-
-    private static void clientThread(){
-
-    }
-
-    public static void startServer(){
-        SERVER = true;
+    /**
+     * starting the server.
+     * waiting endlessly to connect.
+     * when connected, precede
+     */
+    public static void startServerReceiver(){
         new Thread(new Runnable() {
             @Override
             public void run() {
+                SERVER = true;
+                DatagramPacket packet = new DatagramPacket(new byte[1024],PORT);
                 try {
-                    serverSocket = new ServerSocket(PORT);
-                    clientSocket = serverSocket.accept();
-                    serverThread(clientSocket);
-                }catch (Exception e){
-                    Log.i(TAG_SERVER,e.getMessage());
-                }
-            }
-        }).start();
-    }
-
-
-    private static void serverThread(final Socket client){
-        new Thread(new Runnable() {
-
-            private BufferedReader input;
-
-            @Override
-            public void run() {
-
-                try {
-                    input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                }catch (Exception e){
-                    Log.i(TAG_SERVER,e.getMessage());
-                }
-
-                while(SERVER_RUNNING){
-                    try{
-                        Protocol.clientMsg(input.readLine());
-                    }catch (Exception e){
-                        Log.i(TAG_SERVER,e.getMessage());
+                    socketReceive = new DatagramSocket(PORT);
+                    while(true){
+                        socketReceive.receive(packet);
+                        Protocol.clientMsg(packet);
                     }
+                }catch (Exception e){
+                    Log.i(TAG_SERVER,e.getMessage());
                 }
             }
         }).start();
     }
 
+
+    /**
+     *
+     */
+    public static void send(String msg){
+        byte[] data = msg.getBytes();
+        DatagramPacket packet = new DatagramPacket(data,data.length,partnerAddress,PORT);
+        try {
+            socket.send(packet);
+        }catch(Exception e){
+            Log.i((SERVER) ? TAG_SERVER : TAG_CLIENT,e.getMessage());
+        }
+    }
+
+    /**
+     * starting client
+     * broadcasting packets endlessly
+     * when connected, precede, end thread
+     */
+    public static void startClient(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socket = new DatagramSocket(PORT);
+                    InetAddress broadcast = InetAddress.getByName("255.255.255.255");
+                    byte[] data = "CLIENT;TEST".getBytes();
+                    DatagramPacket packet = new DatagramPacket(data,data.length,broadcast,PORT);
+
+                    while(!CLIENT_CONNECTED) {
+                        socket.send(packet);
+                        Thread.sleep(100);
+                    }
+                }catch (Exception e){
+                    Log.i(TAG_CLIENT,e.getMessage());
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * start client receiver
+     */
+    public static void startClientReceiver(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    socketReceive = new DatagramSocket(PORT);
+                    DatagramPacket packet = new DatagramPacket(new byte[1024],PORT);
+                    while(true){
+                        socketReceive.receive(packet);
+                        Protocol.serverMsg(packet);
+                    }
+                }catch(Exception e){
+                    Log.i(TAG_CLIENT,e.getMessage());
+                }
+            }
+        }).start();
+    }
 }
